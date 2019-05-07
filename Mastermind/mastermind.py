4298f9cca9a480580f3869cc076b502511a6f784
Glen Thomas <mail@glenthomas.uk>
2019-05-07 13:34:27 +0100

    SHow instructions and generate a code. Convert score function to use strings instead of lists.

--- Mastermind/mastermind.py
+++ Mastermind/mastermind.py
@@ -1,11 +1,12 @@
 """Game of Mastermind, challenging the user to guess a four item
 code from a simple scoring system."""
-# from random import choices
+import random
 
 
 def score(code, guess):
     """Return list of black and white pegs for correctly coloured pegs in
     the correct location (black) or incorrect location (white)"""
+    code, guess = list(code), list(guess)
     # check guesses in the right place
     black = 0
     for c, g in zip(code, guess):
@@ -17,4 +18,24 @@ def score(code, guess):
             code.remove(c)
             white += 1
     white = white - black     # remove duplicates
-    return list('b' * black + 'w' * white)
+    return "".join(list('b' * black + 'w' * white))
+
+def show_instructions():
+    """Print game instructions"""
+    print("""Mastermind
+
+    Guess the code. The computer will make a four colour code that you
+    cat try to guess. For each colour you get in the right location, you 
+    will see one 'black' peg. For the correct colour in the wron place, 
+    a 'white' peg. 
+    """)
+
+
+def main():
+    colours = list("roygbv")
+    show_instructions()
+    code = random.choices(colours, k=4)
+    print(code)
+
+if __name__ == '__main__':
+    main()
--- Mastermind/test_mastermind.py
+++ Mastermind/test_mastermind.py
@@ -3,10 +3,10 @@ import pytest
 
 
 def test_scores():
-    assert m.score(list("royg"), list("royg")) == list("bbbb")
-    assert m.score(list("roro"), list("rrgr")) == list("bw")
-    assert m.score(list("royg"), list("rogy")) == list("bbww")
-    assert m.score(list("royg"), list("oryg")) == list("bbww")
-    assert m.score(list("oygr"), list("royg")) == list("wwww")
-    assert m.score(list("royg"), list("rrgr")) == list("bw")
-    assert m.score(list("rrrg"), list("yogr")) == list("ww")
+    assert m.score("royg", "royg") == "bbbb"
+    assert m.score("roro", "rrgr") == "bw"
+    assert m.score("royg", "rogy") == "bbww"
+    assert m.score("royg", "oryg") == "bbww"
+    assert m.score("oygr", "royg") == "wwww"
+    assert m.score("royg", "rrgr") == "bw"
+    assert m.score("rrrg", "yogr") == "ww"
