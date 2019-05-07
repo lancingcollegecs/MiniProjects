52888eaf2b23bcbdf530a977548d95a891623446
Glen Thomas <mail@glenthomas.uk>
2019-05-07 21:01:00 +0100

    Working game.

--- Mastermind/mastermind.py
+++ Mastermind/mastermind.py
@@ -1,7 +1,7 @@
 """Game of Mastermind, challenging the user to guess a four item
 code from a simple scoring system."""
 import random
-
+MAX_GUESSES = 10
 
 def score(code, guess):
     """Return list of black and white pegs for correctly coloured pegs in
@@ -26,16 +26,35 @@ def show_instructions():
 
     Guess the code. The computer will make a four colour code that you
     cat try to guess. For each colour you get in the right location, you 
-    will see one 'black' peg. For the correct colour in the wron place, 
+    will see one 'black' peg. For the correct colour in the wrong place, 
     a 'white' peg. 
     """)
+
+def print_history(guesses, marks):
+    """Prints the history of the guesses and the associated marks."""
+    print('\n================\n  guess marks')
+    for attempt, (g, m) in enumerate(zip(guesses, marks)):
+        print(attempt + 1, g, m)
 
 
 def main():
     colours = list("roygbv")
+    guesses = []
+    marks = []
     show_instructions()
-    code = random.choices(colours, k=4)
-    print(code)
+    code = "".join(random.choices(colours, k=4))
+    for attempt in range(MAX_GUESSES):
+        guess = input("\nNext guess: ")
+        mark = score(code, guess)
+        marks.append(mark)
+        guesses.append(guess)
+        print_history(guesses, marks)
+        if mark == "bbbb":
+            print("Well done, you win!")
+            break
+    else: 
+        print("The code was:", code)
+
 
 if __name__ == '__main__':
     main()
