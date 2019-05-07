c8d2bf7efdc466e6d08d0559627ec3742424b7d8
Glen Thomas <mail@glenthomas.uk>
2019-05-07 21:42:52 +0100

    Code tidy.

--- Mastermind/mastermind.py
+++ Mastermind/mastermind.py
@@ -1,7 +1,9 @@
 """Game of Mastermind, challenging the user to guess a four item
 code from a simple scoring system."""
 import random
+
 MAX_GUESSES = 10
+
 
 def score(code, guess):
     """Return list of black and white pegs for correctly coloured pegs in
@@ -9,41 +11,46 @@ def score(code, guess):
     code, guess = list(code), list(guess)
     # check guesses in the right place
     black = 0
-    for c, g in zip(code, guess):
-        if c == g:
+    for code_peg, guess_peg in zip(code, guess):
+        if code_peg == guess_peg:
             black += 1
     white = 0
-    for c in guess:
-        if c in code:
-            code.remove(c)
+    for colour in guess:
+        if colour in code:
+            code.remove(colour)
             white += 1
-    white = white - black     # remove duplicates
-    return "".join(list('b' * black + 'w' * white))
+    white = white - black  # remove duplicates
+    return "".join(list("b" * black + "w" * white))
+
 
 def show_instructions():
     """Print game instructions"""
-    print("""Mastermind
+    print(
+        """Mastermind
 
     Guess the code. The computer will make a four colour code that you
-    cat try to guess. For each colour you get in the right location, you 
-    will see one 'black' peg. For the correct colour in the wrong place, 
-    a 'white' peg. 
-    """)
+    can try to guess. For each colour you get in the right location, you
+    will see one 'black' peg. For the correct colour in the wrong place,
+    a 'white' peg.
+    """
+    )
+
 
 def print_history(guesses, marks):
     """Prints the history of the guesses and the associated marks."""
-    print('\n================\n  guess marks')
-    for attempt, (g, m) in enumerate(zip(guesses, marks)):
-        print(attempt + 1, g, m)
+    print("\n================\n  guess marks")
+    for attempt, (guess, mark) in enumerate(zip(guesses, marks)):
+        print(attempt + 1, guess, mark)
 
 
 def main():
+    """Main game loop."""
     colours = list("roygbv")
     guesses = []
     marks = []
     show_instructions()
     code = "".join(random.choices(colours, k=4))
-    for attempt in range(MAX_GUESSES):
+    for _ in range(MAX_GUESSES):
         guess = input("\nNext guess: ")
         mark = score(code, guess)
         marks.append(mark)
@@ -52,9 +59,9 @@ def main():
         if mark == "bbbb":
             print("Well done, you win!")
             break
-    else: 
+    else:
         print("The code was:", code)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
