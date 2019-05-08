f945f13d0ed4763f5922b89680bf433c29d869c3
Glen Thomas <mail@glenthomas.uk>
2019-05-08 15:34:30 +0100

    Split up the one test functions into four.

--- Mastermind/test_mastermind.py
+++ Mastermind/test_mastermind.py
@@ -1,12 +1,17 @@
 import mastermind as m
-import pytest
 
 
-def test_scores():
+def test_scores_blackPegs():
     assert m.score("royg", "royg") == "bbbb"
+    assert m.score("royg", "royb") == "bbb"
+def test_scores_whitePegs():
+    assert m.score("royg", "bbgy") == "ww"
+    assert m.score("royg", "oygr") == "wwww"
+def test_scores_mixedPegs():
     assert m.score("roro", "rrgr") == "bw"
-    assert m.score("royg", "rogy") == "bbww"
-    assert m.score("royg", "oryg") == "bbww"
-    assert m.score("oygr", "royg") == "wwww"
     assert m.score("royg", "rrgr") == "bw"
-    assert m.score("rrrg", "yogr") == "ww"
+    assert m.score("rrrg", "ygro") == "bw"
+def test_scores_erronious():
+    assert m.score("rrrr", "rrrrr") == "bbbb"
+    assert m.score("royy", "roy") == "bbb"
+    assert m.score("royg", "") == ""
